4dc5e6e53a36a144125391debe0433585aaabd85
Avdi Grimm <avdi@avdi.org>
2013-05-28 17:22:45 -0400

    Add conversion functions.

--- lib/naught.rb
+++ lib/naught.rb
@@ -153,6 +153,8 @@ module Naught
         inspect_proc = @inspect_proc 
         subject.module_eval do
           define_method(:inspect, &inspect_proc)
+          def initialize(*)
+          end
         end
       end
       defer(class: true) do |subject|
@@ -165,16 +167,64 @@ module Naught
         end
       end
     end
+    def null_equivalents
+      @null_equivalents ||= [nil]
+    end
+    def generate_conversions_module(null_class)
+      null_equivs = null_equivalents # get a local bunding
+      @conversions_module ||= Module.new do
+        define_method(:Null) do |object=:nothing_passed|
+          case object
+          when NullObjectTag then object
+          when :nothing_passed, *null_equivs
+            null_class.get(caller: caller(1))
+          else raise ArgumentError, "#{object.inspect} is not null!"
+          end
+        end
+        define_method(:Maybe) do |object=nil, &block|
+          object = block ? block.call : object
+          case object
+          when NullObjectTag then object
+          when *null_equivs
+            null_class.get(caller: caller(1))
+          else
+            object
+          end
+        end
+        
+        define_method(:Just) do |object=nil, &block|
+          object = block ? block.call : object
+          case object
+          when NullObjectTag, *null_equivs
+            raise ArgumentError, "Null value: #{object.inspect}"
+          else
+            object
+          end
+        end
+        
+        define_method(:Actual) do |object=nil, &block|
+          object = block ? block.call : object
+          case object
+          when NullObjectTag then nil
+          else
+            object
+          end
+        end
+      end
+    end
     def generate_class
       generation_mod    = Module.new
       customization_mod = customization_module # get a local binding  
+      builder           = self
       @operations.each do |operation|
         operation.call(generation_mod)
       end
       null_class = Class.new(@base_class) do
         const_set :GeneratedMethods, generation_mod
         const_set :Customizations, customization_mod
+        const_set :Conversions, builder.generate_conversions_module(self)
     
+        include NullObjectTag
         include generation_mod
         include customization_mod
       end
@@ -191,5 +241,7 @@ module Naught
       builder.respond_to_any_message
     end
     builder.generate_class
+  end
+  module NullObjectTag
   end
 end
--- spec/naught_spec.rb
+++ spec/naught_spec.rb
@@ -316,4 +316,114 @@ module Naught
         ])
     end
   end
+  ConvertableNull = Naught.build do |b|
+    b.null_equivalents << ""
+    b.traceable
+  end
+  
+  describe 'Null()' do
+    include ConvertableNull::Conversions
+  
+    specify 'given no input, returns a null object' do
+      expect(Null().class).to be(ConvertableNull)
+    end
+  
+    specify 'given nil, returns a null object' do
+      expect(Null(nil).class).to be(ConvertableNull)
+    end
+  
+    specify 'given a null object, returns the same null object' do
+      null = ConvertableNull.get
+      expect(Null(null)).to be(null)
+    end
+  
+    specify 'given anything in null_equivalents, return a null object' do
+      expect(Null("").class).to be(ConvertableNull)
+    end
+  
+    specify 'given anything else, raises an ArgumentError' do
+      expect{Null(false)}.to raise_error(ArgumentError)
+      expect{Null("hello")}.to raise_error(ArgumentError)
+    end
+  
+    it 'generates null objects with useful trace info' do
+      null = Null(); line = __LINE__
+      expect(null.__line__).to eq(line)
+      expect(null.__file__).to eq(__FILE__)
+    end
+  
+  end
+  describe 'Maybe()' do
+    include ConvertableNull::Conversions
+  
+    specify 'given nil, returns a null object' do
+      expect(Maybe(nil).class).to be(ConvertableNull)
+    end
+  
+    specify 'given a null object, returns the same null object' do
+      null = ConvertableNull.get
+      expect(Maybe(null)).to be(null)
+    end
+  
+    specify 'given anything in null_equivalents, return a null object' do
+      expect(Maybe("").class).to be(ConvertableNull)
+    end
+  
+    specify 'given anything else, returns the input unchanged' do
+      expect(Maybe(false)).to be(false)
+      str = "hello"
+      expect(Maybe(str)).to be(str)
+    end
+  
+    it 'generates null objects with useful trace info' do
+      null = Maybe(); line = __LINE__
+      expect(null.__line__).to eq(line)
+      expect(null.__file__).to eq(__FILE__)
+    end  
+   
+    it 'also works with blocks' do
+      expect(Maybe{nil}.class).to eq(ConvertableNull)
+      expect(Maybe{"foo"}).to eq("foo")
+    end
+  end
+  describe 'Just()' do
+    include ConvertableNull::Conversions
+  
+    specify 'passes non-nullish values through' do
+      expect(Just(false)).to be(false)
+      str = "hello"
+      expect(Just(str)).to be(str)
+    end
+  
+    specify 'rejects nullish values' do
+      expect{Just(nil)}.to raise_error(ArgumentError)
+      expect{Just("")}.to raise_error(ArgumentError)
+      expect{Just(ConvertableNull.get)}.to raise_error(ArgumentError)
+    end
+  
+    it 'also works with blocks' do
+      expect{Just{nil}.class}.to raise_error(ArgumentError)
+      expect(Just{"foo"}).to eq("foo")
+    end
+  end
+  describe 'Actual()' do
+    include ConvertableNull::Conversions
+  
+    specify 'given a null object, returns nil' do
+      null = ConvertableNull.get
+      expect(Actual(null)).to be_nil
+    end
+  
+    specify 'given anything else, returns the input unchanged' do
+      expect(Actual(false)).to be(false)
+      str = "hello"
+      expect(Actual(str)).to be(str)
+      expect(Actual(nil)).to be_nil
+    end
+  
+    it 'also works with blocks' do
+      expect(Actual{ConvertableNull.new}).to be_nil
+      expect(Actual{"foo"}).to eq("foo")
+    end
+  end
 end
