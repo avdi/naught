ae6206db522a8cc35ed14d0643065b36ebce2f3a
Erik Michaels-Ober <sferik@gmail.com>
2014-01-09 11:49:59 +0100

    Refactor ConversionsModule to be an actual Module
    ConversionsModule was previously a class that needed to be initialized
    with two arguments: null_class and null_equivs.
    
    Instead, these arguments are passed via Module#included by setting a
    constant on the null class, including the module in the null class, and
    then removing the constant and undefining the included methods.

--- lib/naught/null_class_builder.rb
+++ lib/naught/null_class_builder.rb
@@ -1,5 +1,5 @@
 require 'naught/basic_object'
-require 'naught/null_class_builder/conversions_module'
+require 'naught/conversions'
 
 module Naught
   class NullClassBuilder
@@ -34,10 +34,6 @@ module Naught
       @null_equivalents ||= [nil]
     end
 
-    def generate_conversions_module(null_class)
-      ConversionsModule.new(null_class, null_equivalents)
-    end
-
     def generate_class
       respond_to_any_message unless interface_defined?
       generation_mod    = Module.new
@@ -49,7 +45,13 @@ module Naught
       null_class = Class.new(@base_class) do
         const_set :GeneratedMethods, generation_mod
         const_set :Customizations, customization_mod
-        const_set :Conversions, builder.generate_conversions_module(self)
+        const_set :NULL_EQUIVS, builder.null_equivalents
+        include Conversions
+        remove_const :NULL_EQUIVS
+        Conversions.instance_methods.each do |instance_method|
+          undef_method(instance_method)
+        end
+        const_set :Conversions, Conversions
 
         include NullObjectTag
         include generation_mod
