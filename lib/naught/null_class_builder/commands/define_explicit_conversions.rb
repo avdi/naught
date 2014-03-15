17f4dcaf1fd190e7df94072f832f8c166db30011
Erik Michaels-Ober <sferik@gmail.com>
2014-03-15 20:14:14 +0100

    Use nested module/class definitions instead of compact style

--- lib/naught/null_class_builder/commands/define_explicit_conversions.rb
+++ lib/naught/null_class_builder/commands/define_explicit_conversions.rb
@@ -1,13 +1,17 @@
 require 'forwardable'
 require 'naught/null_class_builder/command'
 
-module Naught::NullClassBuilder::Commands
-  class DefineExplicitConversions < ::Naught::NullClassBuilder::Command
-    def call
-      defer do |subject|
-        subject.module_eval do
-          extend Forwardable
-          def_delegators :nil, :to_a, :to_c, :to_f, :to_h, :to_i, :to_r, :to_s
+module Naught
+  class NullClassBuilder
+    module Commands
+      class DefineExplicitConversions < ::Naught::NullClassBuilder::Command
+        def call
+          defer do |subject|
+            subject.module_eval do
+              extend Forwardable
+              def_delegators :nil, :to_a, :to_c, :to_f, :to_h, :to_i, :to_r, :to_s
+            end
+          end
         end
       end
     end
--- lib/naught/null_class_builder/commands/singleton.rb
+++ lib/naught/null_class_builder/commands/singleton.rb
@@ -1,20 +1,24 @@
 require 'naught/null_class_builder/command'
 
-module Naught::NullClassBuilder::Commands
-  class Singleton < Naught::NullClassBuilder::Command
-    def call
-      defer(:class => true) do |subject|
-        require 'singleton'
-        subject.module_eval do
-          include ::Singleton
+module Naught
+  class NullClassBuilder
+    module Commands
+      class Singleton < Naught::NullClassBuilder::Command
+        def call
+          defer(:class => true) do |subject|
+            require 'singleton'
+            subject.module_eval do
+              include ::Singleton
 
-          def self.get(*)
-            instance
-          end
+              def self.get(*)
+                instance
+              end
 
-          %w(dup clone).each do |method_name|
-            define_method method_name do
-              self
+              %w[dup clone].each do |method_name|
+                define_method method_name do
+                  self
+                end
+              end
             end
           end
         end
